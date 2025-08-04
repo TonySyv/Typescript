878f266dac346ecf0ca1bc7cc9fd947d06bbe5f2
Tony Syv <11375364+TonySyv@users.noreply.github.com>
2025-08-04 23:04:29 +0800

    SumOfNaturalNumbers.ts

---
+++ SumOfNaturalNumbers.ts
@@ -0,0 +1,13 @@
+// Program to display the sum of natural numbers
+
+let sum: number = 0;
+const n: number = 100
+
+// Loop from i = 1 to i = n
+// in each iteration, i is increased by 1
+for (let i: number = 1; i <= n; i++) {
+    sum += i;  // sum = sum + i
+}
+
+
+console.log(Sum: ${sum});
